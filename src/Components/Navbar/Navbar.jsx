ff9a3dc33b3f9d028b6d2d70d251faf67c8d5689
Rohitkarki-hub <rohitkarki04@gmail.com>
2023-12-15 00:32:58 +0545

    ..

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,12 +6,15 @@ import img from "./imgs/logo.jpg";
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   const navigate = useNavigate();
-
+  console.log(navbarOpen);
   return (
-    <nav className="sticky w-screen top-0  z-50 bg-blue-300 h-20  ">
+    <nav className="fixed w-screen top-0  z-50 bg-blue-300 h-20  ">
       <div className="container mx-auto flex flex-wrap items-center justify-between px-6 ">
         <div className="w-full  sticky top-0 flex justify-between  lg:w-auto lg:static lg:justify-start h-[7vh]  items-center">
-          <a onClick={() => navigate("/")}>
+          <a
+            className="font-medium tracking-wider transition-colors cursor-pointer "
+            onClick={() => navigate("/")}
+          >
             <img src={img} alt="" className="w-11" />
           </a>
           <div className="flex items-center">
@@ -27,14 +30,30 @@ const Navbar = () => {
             </button>
           </div>
         </div>
-        <div>
-          <ul>
-            <li>Trips</li>
-            <li>Recomended Islands</li>
-            <li>Transport</li>
-            <li>Boat Charter</li>
-            <li>Boat Time Tab</li>
-            <li></li>
+
+        <div
+          className={
+            "lg:flex flex-row items-center lg:h-auto h-screen bg-blue-300 cursor-pointer justify-center " +
+            (navbarOpen ? " flex " : "  hidden ")
+          }
+        >
+          <ul className="flex flex-col lg:flex-row items-center gap-x-4 list-none lg:ml-auto lg:transition-y-[50%] lg:gap-y-0 gap-y-8">
+            <li className="py2 flex text-yellow-700 uppercase font-medium items-center leading-snug hover:text-[#1460AD]">
+              Trips
+            </li>
+            <li className="py2 flex text-yellow-700 uppercase font-medium items-center leading-snug hover:text-[#1460AD]">
+              Recomended Islands
+            </li>
+            <li className="py2 flex text-yellow-700 uppercase font-medium items-center leading-snug hover:text-[#1460AD]">
+              Transport
+            </li>
+            <li className="py2 flex text-yellow-700 uppercase font-medium items-center leading-snug hover:text-[#1460AD]">
+              Boat Charter
+            </li>
+            <li className="py2 flex text-yellow-700 uppercase font-medium items-center leading-snug hover:text-[#1460AD]">
+              Boat Time Tab
+            </li>
+            <li className="py2 flex text-yellow-700 uppercase font-medium items-center leading-snug hover:text-[#1460AD]"></li>
           </ul>
         </div>
       </div>
